b7775fb4f1ac5060c6fb7ab59d1a9990b1504900
André Pestana Silva <atlas.ifpa@gmail.com>
2022-10-03 15:06:42 -0300

    01 - Criando teste do que foi feito na página de login

---
+++ src/tests/Login.test.js
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouter } from './helpers/renderWith';
+import App from '../App';
+
+describe('Teste da página de login', () => {
+  beforeEach(() => renderWithRouter(<App />, '/'));
+
+  const CORRECT_EMAIL = 'email.valid@gmail.com';
+  const CORRECT_PASSWORD = 'validPassword';
+  const INCORRECT_EMAIL = 'com.valid@email-gmail';
+  const INCORRECT_PASSWORD = 'pass';
+
+  test('Testa a validação da página de login', () => {
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginButton = screen.getByRole('button');
+
+    userEvent.type(emailInput, INCORRECT_EMAIL);
+    userEvent.type(passwordInput, CORRECT_PASSWORD);
+    expect(loginButton).toHaveProperty('disabled', true);
+
+    userEvent.clear(emailInput);
+    userEvent.clear(passwordInput);
+
+    userEvent.type(emailInput, CORRECT_EMAIL);
+    userEvent.type(passwordInput, INCORRECT_PASSWORD);
+    expect(loginButton).toHaveProperty('disabled', true);
+
+    userEvent.clear(emailInput);
+    userEvent.clear(passwordInput);
+
+    userEvent.type(emailInput, CORRECT_EMAIL);
+    userEvent.type(passwordInput, CORRECT_PASSWORD);
+    expect(loginButton).toHaveProperty('disabled', false);
+    userEvent.click(loginButton);
+  });
+});
